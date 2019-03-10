53b0ac140d166842c3ce998c77b5478429bb6711
Boyer Jérémy <jereboyer08@gmail.com>
2019-03-10 19:32:44 +0100

    Create first view

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerController extends AbstractController{
 
-    
+    /**
+     * @Route("/", name="customer")
+     */
     public function customer(){
 
     }
