2cd3e8f876761a793f8884a8270d500f8a21e53d
Boyer Jérémy <jereboyer08@gmail.com>
2019-03-17 14:24:43 +0100

    Add migration sin relation

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -6,12 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CustomerController extends AbstractController{
+class CustomerController extends AbstractController
+{
 
     /**
      * @Route("/", name="customer")
      */
-    public function customer(){
+    public function customer(Response $response)
+    {
+
 
     }
 
