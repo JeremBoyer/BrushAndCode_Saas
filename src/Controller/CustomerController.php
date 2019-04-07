3e6c5bda891cf2b1225a44ea1297eb69a76018b9
Boyer Jérémy <jereboyer08@gmail.com>
2019-04-07 15:44:43 +0200

    Override registration. Add management asset with webpack.

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Quotation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CustomerController extends AbstractController
@@ -12,9 +14,17 @@ class CustomerController extends AbstractController
     /**
      * @Route("/", name="customer")
      */
-    public function customer(Response $response)
+    public function customer(Request $resquest)
     {
+        $quotation = new Quotation() ;
 
+        $quotationForm = $this->createFormBuilder($quotation)
+                            ->add('comment', TextareaType::class)
+                            ->getForm();
+
+        return $this->render('customer/customer.html.twig' , [
+            'quotationForm' => $quotationForm->createView()
+        ]);
 
     }
 
--- src/Entity/Quotation.php
+++ src/Entity/Quotation.php
@@ -66,6 +66,11 @@ class Quotation
      */
     private $user;
 
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $Email;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -187,6 +192,18 @@ class Quotation
     public function setUser(?User $user): self
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
 
         return $this;
     }
