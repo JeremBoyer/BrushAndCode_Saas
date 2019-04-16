7bd0d72843a3f60548f0ffcb9d79d71cf149d718
Boyer Jérémy <jereboyer08@gmail.com>
2019-04-16 14:52:24 +0200

    Create a pdf with quotationForm, but no style. And script is stop when pdf DL.

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -5,12 +5,10 @@ namespace App\Controller;
 use App\Entity\Quotation;
 use App\Form\QuotationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use App\Service\CreatePdfService;
 
 class CustomerController extends AbstractController
 {
@@ -18,20 +16,28 @@ class CustomerController extends AbstractController
     /**
      * @Route("/", name="customer")
      */
-    public function customer(Request $resquest)
+    public function customer(
+        Request $resquest,
+        CreatePdfService $pdf
+    )
     {
         $quotation = new Quotation() ;
 
         $quotationForm = $this->createForm(QuotationType::class, $quotation);
         $quotation->setCreatedAt(new \DateTime());
-
         $quotation->setStatus(Quotation::TOANSWER);
 
         $quotationForm->handleRequest($resquest);
 
-        dump($quotation);
+        $html = $this->renderView('default/mypdf.html.twig', [
 
-        dump($quotation->getPackageType());
+            'date' => $quotation->getCreatedAt()->format("d/m/Y"),
+            'email' => $quotation->getEmail(),
+            'comment' => $quotation->getComment()
+
+        ]);
+        $pdf->createPdf($html);
+
 
         if ($quotationForm->isSubmitted() && $quotationForm->isValid())
         {
---
+++ src/Service/CreatePdfService.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Service;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Bundle\FrameworkBundle\Controller\ControllerTrait;
+
+class CreatePdfService
+{
+
+    public function createPdf(
+        $html
+    )
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => true
+        ]);
+
+    }
+
+
+
+
+}
