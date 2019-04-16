3c0b6359f82a79a7c57f276512bba1d0884248c9
Boyer Jérémy <jereboyer08@gmail.com>
2019-04-16 14:52:24 +0200

    Create a pdf with quotationForm, but no style. And script is stop when pdf DL.

---
+++ src/Service/CreatePdfService.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Service;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+class CreatePdfService
+{
+
+    public function createPdf(
+        $html
+    )
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("mypdf.pdf", [
+            "Attachment" => true
+        ]);
+
+    }
+}
